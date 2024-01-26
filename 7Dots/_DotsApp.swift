206f699ca0e0ffabae640d63cc2bd183433df547
shomil03 <shomil02020@gmail.com>
2024-01-26 15:29:44 +0530

    Initial Commit

---
+++ 7Dots/_DotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DotsApp.swift
+//  7Dots
+//
+//  Created by Shomil Singh on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct _DotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
